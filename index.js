fb4ec5e8f444e86934a68ce5a23e7227d9162b95
Pranay Narang <pranaynarang@gmail.com>
2021-09-09 19:06:39 +0530

    Drop Firebase support

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('morgan')
-const admin = require("firebase-admin");
 const cors = require("cors")
 
 const CONFIG = require('./config/config')
 const routes = require('./routes')
-
-const firebaseServiceAccount = {
-    type: CONFIG.FIREBASE_ACCOUNT_TYPE,
-    project_id: CONFIG.FIREBASE_PROJECT_ID,
-    private_key_id: CONFIG.FIREBASE_PRIVATE_KEY_ID,
-    private_key: CONFIG.FIREBASE_PRIVATE_KEY,
-    client_email: CONFIG.FIREBASE_CLIENT_EMAIL,
-    client_id: CONFIG.FIREBASE_CLIENT_ID,
-    auth_uri: CONFIG.FIREBASE_AUTH_URI,
-    token_uri: CONFIG.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: CONFIG.FIREBASE_AUTH_PROVIDER_x509_CERT_URL,
-    client_x509_cert_url: CONFIG.FIREBASE_CLIENT_x509_CERT_URL
-}
 
 const mongoConnectionURI = CONFIG.DB_PASSWORD && CONFIG.DB_USERNAME ?
     `mongodb://${CONFIG.DB_USERNAME}:${CONFIG.DB_PASSWORD}@${CONFIG.DB_URL}/${CONFIG.DB_NAME}?authSource=admin` :
@@ -31,10 +17,6 @@ mongoose.connect(mongoConnectionURI, {
     useFindAndModify: true,
     useCreateIndex: true
 })
-
-admin.initializeApp({
-    credential: admin.credential.cert(firebaseServiceAccount)
-});
 
 const app = express()
 app.use(express.json())
