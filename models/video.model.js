eb57ed3690ea2be76b5b2b76b12f92a4aae0bcb8
Pranay Narang <pranaynarang@gmail.com>
2021-09-10 00:07:59 +0530

    models: videos: Index name and description fields

--- models/video.model.js
+++ models/video.model.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
-    title: {type: String, index: true},
-    description: {type: String, index: true},
+    title: String,
+    description: String,
     publishedAt: Date,
     thumbnails: Array
 })
+
+schema.index({name: 'text', description: 'text'}) // Index name and description fields to allow performing search
 
 const Video = mongoose.model("Video", schema)
 
